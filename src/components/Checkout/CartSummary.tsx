0c1ad8d022fae41788779d32562e75a90e974528
Filip <filip.knias@o2.pl>
2022-07-11 12:35:32 +0200

    add props to cart summary component instead of getting data from redux state

--- src/components/Checkout/CartSummary.tsx
+++ src/components/Checkout/CartSummary.tsx
@@ -1,27 +1,26 @@
 import { FC } from 'react';
-import { useAppSelector } from '../../redux/hooks';
+import { LineItem } from '@chec/commerce.js/types/line-item';
 
-const CartSummary: FC = () => {
-  const { cart } = useAppSelector((state) => state.cart);
+interface Props {
+  items: LineItem[];
+  totalPrice: string;
+}
 
+const CartSummary: FC<Props> = ({ items, totalPrice }) => {
   return (
     <>
-      {cart && (
-        <>
-          {cart.line_items.map(({ id, name, price, quantity }) => (
-            <div key={id}>
-              <div className="flex items-center justify-between font-medium border-b-2 py-4">
-                <h2>{name}</h2>
-                <h2>{price.formatted_with_symbol} ({quantity})</h2>
-              </div>
-            </div>
-          ))}
-          <div className="flex items-center justify-between font-bold border-b-2 py-4 text-xl">
-            <h2>Total</h2>
-            <h2>{cart.subtotal.formatted_with_symbol}</h2>
+      {items.map(({ id, name, price, quantity }) => (
+        <div key={id}>
+          <div className="flex items-center justify-between font-medium border-b-2 py-4">
+            <h2>{name}</h2>
+            <h2>{price.formatted_with_symbol} ({quantity})</h2>
           </div>
-        </>
-      )}
+        </div>
+      ))}
+      <div className="flex items-center justify-between font-bold border-b-2 py-4 text-xl">
+        <h2>Total</h2>
+        <h2>{totalPrice}</h2>
+      </div>
     </>
   )
 }
